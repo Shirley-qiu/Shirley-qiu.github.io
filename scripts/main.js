f1ce82e7453c46bac0e94345ad3f2f94a89804f5
Shirley Qiu <46514410+Shirley-qiu@users.noreply.github.com>
2020-04-29 00:26:51 -0400

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,10 @@
+<script>
+  function navBarFunction() {
+    var x = document.getElementById("navBar");
+    if (x.className === "navBar") {
+      x.className += " responsive";
+    } else {
+      x.className = "navBar";
+    }
+  }
+</script>
